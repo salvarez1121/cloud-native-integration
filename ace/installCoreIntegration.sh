056043c5e08bbeab28b69e023b98f1a47e2fba39
Callum Jackson <callumj@uk.ibm.com>
2022-11-29 14:09:50 +0000

    Initial version

--- ace/installCoreIntegration.sh
+++ ace/installCoreIntegration.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 #Deploy echo Integration Server
 oc apply -f $SCRIPT_DIR/resources/echoIntegrationServer.yaml
--- ace/installInfiniteIntegration.sh
+++ ace/installInfiniteIntegration.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 oc apply -f $SCRIPT_DIR/resources/infiniteIntegrationServer.yaml
