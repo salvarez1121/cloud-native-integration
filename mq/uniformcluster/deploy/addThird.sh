c3cc10c79a65d2e356b5017639b471185d00ac91
Callum Jackson <callumj@uk.ibm.com>
2022-11-30 13:25:22 +0000

    Initial version

--- mq/uniformcluster/deploy/addThird.sh
+++ mq/uniformcluster/deploy/addThird.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
 export TARGET_NAMESPACE=${1:-"cp4i"}
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR
 
 ( echo "cat <<EOF" ; cat addThirdQM.yaml_template ; echo EOF ) | sh > addThirdQM.yaml
 
