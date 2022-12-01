bd2ccad5c8b31c7ee27c1a0f6b8bf007e6e65e05
Callum Jackson <callumj@uk.ibm.com>
2022-12-01 09:20:35 +0000

    Initial version

--- mq/uniformcluster/deploy/scaleCPU.sh
+++ mq/uniformcluster/deploy/scaleCPU.sh
@@ -5,9 +5,9 @@ export TARGET_NAMESPACE=${1:-"cp4i"}
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 oc patch qmgr ucqm1 --patch-file $SCRIPT_DIR/qmgrPatchCPU.yaml -n $TARGET_NAMESPACE --type=merge
-sleep 120s
+sleep 30s
 oc patch qmgr ucqm2 --patch-file $SCRIPT_DIR/qmgrPatchCPU.yaml -n $TARGET_NAMESPACE --type=merge
-sleep 120s
+sleep 30s
 oc patch qmgr ucqm3 --patch-file $SCRIPT_DIR/qmgrPatchCPU.yaml -n $TARGET_NAMESPACE --type=merge
 
 # Don't worry if the last command fails, some of the resources may have already
